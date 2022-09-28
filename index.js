ea06a18d997ba9e5a3ef93f0b69648c2caf976e7
Moringa School <Abdulbasid.sheikh@student.moringaschool>
2022-09-28 15:37:48 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(drivers, name){
+    return drivers.filter((match) => match.toLowerCase() === name.toLowerCase());
+}
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(
+        (maybeMatch) =>
+          maybeMatch.toLowerCase().indexOf(letters.toLowerCase()) === 0
+      );
+}
+function matchName(drivers, nameFind){
+    return drivers.filter((record) => record.name === nameFind);
+}
+
+
+
+
+
+
+
